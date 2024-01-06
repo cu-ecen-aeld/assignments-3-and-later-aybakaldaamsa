14ab8d28c80dcaf150c4bb9c176abdc8b099a857
aybakaldaamsa <aybakaldaamsa2@gmail.com>
2024-01-06 23:21:28 +0300

    assignment-3

--- examples/systemcalls/systemcalls.c
+++ examples/systemcalls/systemcalls.c
@@ -1,5 +1,13 @@
 #include "systemcalls.h"
-
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/stat.h>
+#include <sys/types.h>
+#include <errno.h>
+#include <string.h>
 /**
  * @param cmd the command to execute with system()
  * @return true if the command in @param cmd was executed
@@ -16,8 +24,17 @@ bool do_system(const char *cmd)
  *   and return a boolean true if the system() call completed with success
  *   or false() if it returned a failure
 */
-
-    return true;
+  int returnStatus= system(cmd);
+  if (returnStatus == 0){
+  return true; 
+   }
+   else{
+   return false;
+   }
+   
+   
+   
+   // return true;
 }
 
 /**
@@ -58,10 +75,52 @@ bool do_exec(int count, ...)
  *   as second argument to the execv() command.
  *
 */
+     bool result;
+    pid_t pid = fork();
 
+    if (pid == -1) {
+    printf("Fork failed %s\n",strerror(errno));
+        fflush(stdout);
+        exit(EXIT_FAILURE);
+    }
+
+    if (pid == 0) { // Child process
+    
+     for(i=0; i<count; i++)
+    {
+     printf("CMD : %s\n",command[i]);
+        
+    }
+     fflush(stdout);
+     
+        if(execv(command[0], command) ==-1){
+        perror("Exec failed");
+        exit(EXIT_FAILURE);
+        }
+    } else { // Parent process
+        int status;
+        waitpid(pid, &status, 0); // Wait for the child process to finish
+        
+        if (WIFEXITED(status)) {
+            int exit_status = WEXITSTATUS(status);
+            if (exit_status == 0) {
+                printf("Command executed successfully!\n");
+                result=true;
+            } else {
+                printf("Command execution failed with status: %d\n", exit_status);
+                fflush(stdout);
+                
+            }
+        } else {
+            printf("Command execution failed\n");
+        fflush(stdout);
+                        result= false;
+        }
+        
+    }
     va_end(args);
 
-    return true;
+    return result;
 }
 
 /**
@@ -92,8 +151,80 @@ bool do_exec_redirect(const char *outputfile, int count, ...)
  *   The rest of the behaviour is same as do_exec()
  *
 */
+pid_t pid; 
+
+
+int fd = open(outputfile, O_WRONLY | O_TRUNC | O_CREAT, 0644);
+if (fd <0 ) { 
+    perror("open"); // Print an error message if file opening fails
+    abort(); // Abort the program if file opening fails
+}
+
+pid = fork(); // Fork a child process
+if (pid <0) {
+    printf("Fork failed with %s\n", strerror(errno)); 
+    fflush(stdout); // Flush stdout to ensure the message is displayed
+    return false;
+}
+
+if (pid == 0) { // Child process's code
+
+    // Output the command strings before redirection
+    for (i = 0; i < count; i++) {
+        printf("cmd: %s\n", command[i]);
+    }
+    fflush(stdout); // Flush stdout to ensure the messages are displayed
+
+    // Redirect stdout to the file descriptor obtained earlier
+    if (dup2(fd, 1) < 0) {
+        perror("dup2"); 
+        abort(); 
+    }
+    close(fd); 
+
+
+    int ret = execv(command[0], command);
+    if (ret == -1) {
+        perror("child execv failed"); 
+        return false; 
+    }
+} else { // Parent process's code
+    close(fd); 
+
+    int status;
+    // Wait for the child process to terminate and retrieve its status
+    if (waitpid(pid, &status, 0) == -1) {
+        perror("waitpid() failed"); 
+        return false; 
+    }
+
+    int returned = 0;
+    int signum = 0;
+
+    if (WIFEXITED(status)) {
+        returned = WEXITSTATUS(status);
+        printf("at do_exec_redirect, Exited normally with retCode %d\n", returned);
+    } else if (WIFSIGNALED(status)) {
+        signum = WTERMSIG(status);
+        printf("Exited due to receiving signal %d\n", signum);
+    } else if (WIFSTOPPED(status)) {
+        signum = WSTOPSIG(status);
+        printf("Stopped due to receiving signal %d\n", signum);
+    } else {
+        printf("Something strange just happened.\n");
+    }
+
+    printf("status %d\n", status);
+    printf("Parent received child %d exit status was %d OR signum %d \n", pid, returned, signum);
+    fflush(stdout);
+    return returned == 0;
+}
+
+printf("pid %d at do_exec_redirect end\n", pid); 
+fflush(stdout); // Flush stdout to ensure the message is displayed
 
     va_end(args);
 
     return true;
 }
+//commit
